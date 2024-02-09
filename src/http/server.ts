ddccacb3464d4bc4f9f488c6f4e0e486997756d1
RaulBirlem <raul.birlem@gmail.com>
2024-02-09 14:35:22 -0300

    docker-compose

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,2 +1,11 @@
-console.log("Server");
+import fastify from 'fastify'
 
+const app = fastify()
+
+app.get("/hello", ()=>{
+    return "Hello NLW"
+})
+
+app.listen({port:3333}).then(()=>{
+    console.log("HTTP server running!")
+})
